9fbc08e3721895342cb882d415b7198a2130796e
Sam Bossley <35381535+bossley9@users.noreply.github.com>
2020-11-10 21:48:25 -0500

    Adjust cpp flags to compile for BSD systems only

--- config.mk
+++ config.mk
@@ -1,8 +1,6 @@
 # slock version
 VERSION = 1.4
 
-# Customize below to fit your system
-
 # paths
 PREFIX = /usr/local
 MANPREFIX = ${PREFIX}/share/man
@@ -11,11 +9,13 @@ X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
 
 # includes and libs
-INCS = -I. -I/usr/include -I${X11INC}
-LIBS = -L/usr/lib -lc -lcrypt -L${X11LIB} -lX11 -lXext -lXrandr
+INCS = -I. -I/usr/local/include -I/usr/include -I${X11INC}
+LIBS = -L/usr/local/lib -L/usr/lib -lc -lcrypt -L${X11LIB} -lX11 -lXext -lXrandr
 
 # flags
-CPPFLAGS = -DVERSION=\"${VERSION}\" -D_DEFAULT_SOURCE -DHAVE_SHADOW_H
+# CPPFLAGS = -DVERSION=\"${VERSION}\" -D_DEFAULT_SOURCE -DHAVE_SHADOW_H
+# slock only on BSD laptops
+CPPFLAGS = -DVERSION=\"${VERSION}\" -D_DEFAULT_SOURCE
 CFLAGS = -std=c99 -pedantic -Wall -Os ${INCS} ${CPPFLAGS}
 LDFLAGS = -s ${LIBS}
 COMPATSRC = explicit_bzero.c
