63c68f5f1117a13b4a110a529db65bcf03c20611
Sam Bossley <35381535+bossley9@users.noreply.github.com>
2021-03-29 13:10:13 -0400

    Switch back to linux libs
    this is really inconvenient and I wonder if there's a better solution

--- config.mk
+++ config.mk
@@ -10,15 +10,15 @@ X11LIB = /usr/X11R6/lib
 
 # includes and libs
 INCS = -I. -I/usr/local/include -I/usr/include -I${X11INC}
-# LIBS = -L/usr/local/lib -L/usr/lib -lc -lcrypt -L${X11LIB} -lX11 -lXext -lXrandr
-LIBS = -L/usr/local/lib -L/usr/lib -lc -L${X11LIB} -lX11 -lXext -lXrandr
+LIBS = -L/usr/local/lib -L/usr/lib -lc -lcrypt -L${X11LIB} -lX11 -lXext -lXrandr
+# LIBS = -L/usr/local/lib -L/usr/lib -lc -L${X11LIB} -lX11 -lXext -lXrandr
 
 # flags
 
 # linux
-# CPPFLAGS = -DVERSION=\"${VERSION}\" -D_DEFAULT_SOURCE -DHAVE_SHADOW_H
+CPPFLAGS = -DVERSION=\"${VERSION}\" -D_DEFAULT_SOURCE -DHAVE_SHADOW_H
 # BSD
-CPPFLAGS = -DVERSION=\"${VERSION}\" -D_DEFAULT_SOURCE
+# CPPFLAGS = -DVERSION=\"${VERSION}\" -D_DEFAULT_SOURCE
 
 CFLAGS = -std=c99 -pedantic -Wall -Os ${INCS} ${CPPFLAGS}
 LDFLAGS = -s ${LIBS}
